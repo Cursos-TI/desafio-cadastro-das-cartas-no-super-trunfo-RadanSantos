a68218a3352b001bb24b9d4f5eb0252f02256322
Radan Santos <radansantos@gmail.com>
2025-08-06 05:20:08 +0000

    cartas atualizadas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 struct Carta {
+    char estado[3];
+    char codigo[4];
+    char nomeCidade[50];
     int populacao;
     float area;
     float pib;
@@ -8,8 +11,19 @@ struct Carta {
 };
 int main() {
     struct Carta cartas[2];
+
     for (int i = 0; i < 2; i++) {
         printf("Cadastro da carta %d:\n", i + 1);
+
+        printf("Digite o estado (ex: SP): ");
+        scanf("%s", cartas[i].estado);
+
+        printf("Digite o codigo da cidade (ex: A01): ");
+        scanf("%s", cartas[i].codigo);
+
+        printf("Digite o nome da cidade: ");
+        scanf(" %[^\n]", cartas[i].nomeCidade);
+
         printf("Digite a populacao: ");
         scanf("%d, &cartas[i].populacao");
 
@@ -29,6 +43,9 @@ int main() {
 printf("\n--- Dados das cartas cadastradas ---\n");
 for (int i = 0; i < 2; i++) {
     printf("Carta %d:\n", i + 1);
+    printf("||Estado: %s\n", cartas[i].estado);
+    printf("Codigo da cidade: %s\n", cartas[i].codigo);
+    printf("Nome da cidade: %s\n", cartas[i].nomeCidade);
     printf("Populacao: %d\n", cartas[i].populacao);
     printf("Area: %.2f km²\n", cartas[i].area);
     printf("PIB: %.2f bilhoes\n", cartas[i].pib);
@@ -38,3 +55,4 @@ for (int i = 0; i < 2; i++) {
 
 return 0;
 }
+
